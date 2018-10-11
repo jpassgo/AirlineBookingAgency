64894bb757f159cb569316b4bf72aa911e46944f
Jeff Pascoe <jpascoe@solstice.com>
2018-10-10 21:15:57 -0500

    Initial flights-services spring code project.

---
+++ flights-service/src/main/java/com/personal/pascoe/flightsservice/FlightsServiceApplication.java
@@ -0,0 +1,12 @@
+package com.personal.pascoe.flightsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightsServiceApplication.class, args);
+    }
+}
