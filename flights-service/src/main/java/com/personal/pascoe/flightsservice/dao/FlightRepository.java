e3a603bd5eed5d81096236136c3e860a93584551
Jeff Pascoe <jpascoe@solstice.com>
2018-10-23 20:19:23 -0500

    Add Repository annotation.

--- flights-service/src/main/java/com/personal/pascoe/flightsservice/dao/FlightRepository.java
+++ flights-service/src/main/java/com/personal/pascoe/flightsservice/dao/FlightRepository.java
@@ -2,9 +2,11 @@ package com.personal.pascoe.flightsservice.dao;
 
 import com.personal.pascoe.flightsservice.entity.Flight;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface FlightRepository extends CrudRepository<Flight, Long> {
     List<Flight> findAll();
 }
