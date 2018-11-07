82e997ce5f5900dc231a3a994e77a96dd76a141a
Jeff Pascoe <jpascoe@solstice.com>
2018-11-06 20:27:38 -0600

    Spotless

--- flights-service/src/main/java/com/personal/pascoe/flightsservice/client/UserAccountFeignClient.java
+++ flights-service/src/main/java/com/personal/pascoe/flightsservice/client/UserAccountFeignClient.java
@@ -1,6 +1,5 @@
 package com.personal.pascoe.flightsservice.client;
 
-import com.personal.pascoe.flightsservice.model.UserAccount;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +8,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @FeignClient("userAccounts")
 public interface UserAccountFeignClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/useraccounts/{userAccountId}")
-    UserAccount getUserAccount(@Param("userAccountId") long userAccountId);
+  @RequestMapping(method = RequestMethod.GET, value = "/useraccounts/{userAccountId}")
+  UserAccount getUserAccount(@Param("userAccountId") long userAccountId);
 }
