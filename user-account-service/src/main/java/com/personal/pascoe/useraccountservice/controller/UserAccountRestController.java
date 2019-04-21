acba71981b499751e53fc778080211c481ecf280
jeffpascoe <jeffpascoe9@gmail.com>
2019-04-21 18:45:23 -0500

    Simplifying endpoint name.

--- user-account-service/src/main/java/com/personal/pascoe/useraccountservice/controller/UserAccountRestController.java
+++ user-account-service/src/main/java/com/personal/pascoe/useraccountservice/controller/UserAccountRestController.java
@@ -9,17 +9,23 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/users")
 public class UserAccountRestController {
 
-    UserAccountService userAccountService;
+  UserAccountService userAccountService;
 
-    @GetMapping("/useraccounts/{userAccountId}")
-    public ResponseEntity<UserAccount> getUserAccount(@PathVariable Long userAccountId) {
-        return new ResponseEntity(userAccountService.getUserAccountById(userAccountId), HttpStatus.OK);
-    }
+  @GetMapping("/{userAccountId}")
+  public ResponseEntity<UserAccount> getUserAccount(@PathVariable long userAccountId) {
+    return new ResponseEntity(userAccountService.getUserAccountById(userAccountId), HttpStatus.OK);
+  }
 
-    @PostMapping("/useraccounts")
-    public UserAccount putUserAccount(@RequestBody UserAccount userAccount) {
-        return userAccountService.addUserAccount(userAccount);
-    }
+  @PostMapping()
+  public ResponseEntity<UserAccount> createUserAccount(@RequestBody UserAccount userAccount) {
+    return new ResponseEntity(userAccountService.addUserAccount(userAccount), HttpStatus.CREATED);
+  }
+
+  @DeleteMapping("/{userAccountId}")
+  public ResponseEntity<UserAccount> deleteUserAccount(@PathVariable long userAccountId) {
+    return new ResponseEntity(userAccountService.deleteUserAccount(userAccountId), HttpStatus.OK);
+  }
 }
