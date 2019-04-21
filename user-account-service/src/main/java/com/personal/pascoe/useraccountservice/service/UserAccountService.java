233d6e7852bc27ea028a78bb10b6091f5f5c2148
jeffpascoe <jeffpascoe9@gmail.com>
2019-04-21 18:46:57 -0500

    Adding method to be allow for deletion of users.

--- user-account-service/src/main/java/com/personal/pascoe/useraccountservice/service/UserAccountService.java
+++ user-account-service/src/main/java/com/personal/pascoe/useraccountservice/service/UserAccountService.java
@@ -9,15 +9,21 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class UserAccountService {
 
-    UserAccountRepository userAccountRepository;
+  UserAccountRepository userAccountRepository;
 
-    public UserAccount getUserAccountById(Long userAccountId) {
-        return userAccountRepository.findById(userAccountId).isPresent()
-                ? userAccountRepository.findById(userAccountId).get()
-                : null;
-    }
+  public UserAccount getUserAccountById(long userAccountId) {
+    return userAccountRepository.findById(userAccountId).isPresent()
+        ? userAccountRepository.findById(userAccountId).get()
+        : null;
+  }
 
-    public UserAccount addUserAccount(UserAccount userAccount) {
-        return userAccountRepository.save(userAccount);
-    }
+  public UserAccount addUserAccount(UserAccount userAccount) {
+    return userAccountRepository.save(userAccount);
+  }
+
+  public long deleteUserAccount(long userAccountId) {
+    UserAccount account = getUserAccountById(userAccountId);
+    userAccountRepository.delete(account);
+    return account.getId();
+  }
 }
