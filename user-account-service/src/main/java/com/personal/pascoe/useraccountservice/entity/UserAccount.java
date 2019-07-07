4fd65e508c46dd3e740bf13ca5d150859558585b
jeffpascoe <jeffpascoe9@gmail.com>
2019-07-07 18:57:26 -0500

    Optimizing imports.

--- user-account-service/src/main/java/com/personal/pascoe/useraccountservice/entity/UserAccount.java
+++ user-account-service/src/main/java/com/personal/pascoe/useraccountservice/entity/UserAccount.java
@@ -2,14 +2,13 @@ package com.personal.pascoe.useraccountservice.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonAppend;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
+import java.time.LocalDate;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
--- user-account-service/src/test/java/com/personal/pascoe/useraccountservice/UserAccountRestControllerTest.java
+++ user-account-service/src/test/java/com/personal/pascoe/useraccountservice/UserAccountRestControllerTest.java
@@ -1,5 +1,10 @@
 package com.personal.pascoe.useraccountservice;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import com.personal.pascoe.useraccountservice.controller.UserAccountRestController;
 import com.personal.pascoe.useraccountservice.entity.UserAccount;
 import com.personal.pascoe.useraccountservice.service.UserAccountService;
@@ -8,11 +13,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UserAccountRestControllerTest {
