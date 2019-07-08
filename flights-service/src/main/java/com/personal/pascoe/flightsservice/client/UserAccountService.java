522e5a617a5fa010bb90ad7a42ad7816f86edfcb
jeffpascoe <jeffpascoe9@gmail.com>
2019-07-07 19:00:09 -0500

    Adding @Service annotation.

--- flights-service/src/main/java/com/personal/pascoe/flightsservice/client/UserAccountService.java
+++ flights-service/src/main/java/com/personal/pascoe/flightsservice/client/UserAccountService.java
@@ -3,9 +3,11 @@ package com.personal.pascoe.flightsservice.client;
 import com.personal.pascoe.flightsservice.model.UserAccount;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Service
 public class UserAccountService {
   UserAccountFeignClient userAccountFeignClient;
 
